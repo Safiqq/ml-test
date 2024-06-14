ff93f90602adc7ff3f79255f1aaab581c70713e6
safiqq <szarifin20041@gmail.com>
2024-06-15 02:46:25 +0700

    feat: created gui for batch testing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      <h2>Upload Images</h2>
+      <input type="file" @change="uploadImages" accept=".zip">
+    </div>
+    <div v-if="results.length">
+      <h2>Prediction Results</h2>
+      <ul>
+        <li v-for="(result, index) in results" :key="index">
+          <img :src="result.url" :alt="result.name" style="width: 100px; height: auto;"/>
+          {{ result.name }} --- {{ result.prediction }}: {{ result.confidence }}%
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JSZip from 'jszip'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      results: []
+    }
+  },
+  methods: {
+    async uploadImages(event) {
+      const file = event.target.files[0]
+      const zip = new JSZip()
+      const content = await zip.loadAsync(file)
+      const imageFilesPromises = []
+
+      content.forEach((relativePath, file) => {
+        if (relativePath.endsWith('.jpg') || relativePath.endsWith('.png') || relativePath.endsWith('.jpeg')) {
+          imageFilesPromises.push(file.async('blob').then(blob => ({ relativePath, blob })))
+        }
+      })
+
+      const imageFiles = await Promise.all(imageFilesPromises)
+
+      for (const { relativePath, blob } of imageFiles) {
+        const formData = new FormData()
+        formData.append('image', new File([blob], relativePath, { type: "image/jpeg" }), relativePath)
+
+        // Create a URL for the blob to display the image
+        const imageUrl = URL.createObjectURL(blob)
+
+        const response = await fetch('https://fitfirst-ml-backend-haexo7tjpa-et.a.run.app/food/predict', {
+          method: 'POST',
+          body: formData
+        })
+
+        const prediction = await response.json()
+        this.results.push({
+          name: relativePath,
+          url: imageUrl,  // Store the image URL
+          prediction: prediction.food,
+          confidence: (prediction.confidence).toFixed(2)
+        })
+      }
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://fitfirst-ml-backend-haexo7tjpa-et.a.run.app',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
 })
